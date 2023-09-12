5aec5d9934aa6437877a1391a4cbe99e1a409ed8
Tizy275 <144769191+Tizy275@users.noreply.github.com>
2023-09-12 11:36:07 +0200

    Init

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Willkommen beim Tascherechner");
+
+bool checkParse1 = false;
+bool checkParse2 = false;
+int num1 = 0;
+int num2 = 0;
+
+while (!checkParse1 || !checkParse2)
+{
+   Console.WriteLine("Bitte gebe Zahl 1 an:");
+   var input1 = Console.ReadLine();
+
+   Console.WriteLine("Bitte gebe Zahl 2 an:");
+   var input2 = Console.ReadLine();
+
+   checkParse1 = int.TryParse(input1, out num1);
+   checkParse2 = int.TryParse(input2, out num2);
+
+}
+var result = num1 + num2;
+Console.WriteLine($"{num1} + {num2} = {result}");
